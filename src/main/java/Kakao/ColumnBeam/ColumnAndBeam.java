c21460f1c2248d07fc57832120320e4b13a2e981
JuJinPark <wnwls1216@gmail.com>
2019-10-25 18:18:24 +0900

    added beamAndColumn

--- src/main/java/Kakao/ColumnBeam/ColumnAndBeam.java
+++ src/main/java/Kakao/ColumnBeam/ColumnAndBeam.java
@@ -6,12 +6,18 @@ import java.util.PriorityQueue;
 public class ColumnAndBeam {
 
     static PriorityQueue<Material> constructed;
-    static int[][] map;
+    static int[][] totalMap;
+    static int[][] columnMap;
+    static int[][] beamMap;
+
+
     static int N;
     public int[][] solution(int n, int[][] build_frame) {
       constructed = new PriorityQueue<>(new MaterialComparator ());
       N=n;
-      map= new int[N+1][N+1];
+      totalMap= new int[N+1][N+1];
+      columnMap= new int[N+1][N+1];
+      beamMap= new int[N+1][N+1];
 
         for(int i=0;i< build_frame.length;i++){
 
@@ -30,37 +36,37 @@ public class ColumnAndBeam {
 
 
     private boolean isConstructable(int[] frames){
+        int yinMap=N-frames[1];
+        int xinMap=frames[0];
 
-        if(frames[2]==0) return checkForColumn(frames[0],frames[1]);
+        if(frames[2]==0) return checkForColumn(xinMap,yinMap);
 
-        return checkForBeam(frames[0],frames[1]);
+        return checkForBeam(xinMap,yinMap);
     }
 
-    private boolean checkForColumn(int x,int y){
-        int yinMap=N-y;
-        int xinMap=x;
+    private boolean checkForColumn(int xinMap,int yinMap){
+
 
         if(yinMap==N) return true; //바닥
 
-        if(xinMap-1>0&&map[yinMap][xinMap-1]==2) return true; // 밑에 보
+        if(xinMap-1>=0&&beamMap[yinMap][xinMap-1]==1) return true; // 옆에 보
 
-        if(yinMap-1>0&&map[yinMap-1][xinMap]==1) return true; //밑에 기둥
+        if(beamMap[yinMap][xinMap]==1) return true; // 같은자리에 보
+
+        if(yinMap+1<=N&&columnMap[yinMap+1][xinMap]==1) return true; //밑에 기둥
 
         return false;
     }
 
-    private boolean checkForBeam(int x,int y){
-        int yinMap=N-y;
-        int xinMap=x;
+    private boolean checkForBeam(int xinMap,int yinMap){
 
+        if(yinMap+1<=N&&columnMap[yinMap+1][xinMap]==1) return true;// 바로 밑에 기둥
 
-        if(yinMap-1>0&&map[yinMap-1][xinMap]==1) return true;// 바로 밑에 기둥
+        if(yinMap+1<=N&&xinMap+1<=N&&columnMap[yinMap+1][xinMap+1]==1)  return true;      //옆에 밑에 기둥
 
-        if(yinMap-1>0&&xinMap+1<=N&&map[yinMap-1][xinMap+1]==1)        //옆에 밑에 기둥
-
-
-
-        if(xinMap-1>0&&xinMap+1<=N&&map[yinMap][xinMap-1]==2&&map[yinMap][xinMap+1]==2) return true;    //양옆에 보
+        if(xinMap-1>=0&&xinMap+1<=N&&beamMap[yinMap][xinMap-1]==1&&beamMap[yinMap][xinMap+1]==1) {
+            return true;    //양옆에 보
+        }
 
 
 
@@ -68,36 +74,196 @@ public class ColumnAndBeam {
     }
 
     private boolean isRemovable(int[] frames){
-        if(frames[2]==0) return checkForColumn(frames[0],frames[1]);
+        int yinMap=N-frames[1];
+        int xinMap=frames[0];
 
-        return checkForBeam(frames[0],frames[1]);
+
+        if(frames[2]==0) {
+            return isColumnRemovable(xinMap,yinMap);
+        }
+
+        return isBeamRemovable(xinMap,yinMap);
+
+
+
+
+
+    }
+
+    private boolean isColumnRemovable(int xinMap,int yinMap){
+
+        return checkUp(xinMap,yinMap)&&checkUpLeft(xinMap,yinMap);
+
+    }
+
+    private boolean checkUp(int xinMap,int yinMap){
+
+        int x=xinMap;
+        int y=yinMap-1;
+
+        if(y<0){
+            return true;
+            //위가 없음
+        }
+
+        if(totalMap[y][x]==1){
+            return checkForColumn(x,y);
+            // 위에 기둥일시
+        }else if(totalMap[y][x]==2||totalMap[y][x]==3){
+            return checkForBeam(x,y);
+            //위에 보거나 둘다 있거나
+
+        } else{
+            return true;
+            //위에 아무것도 없음
+        }
+
+
+    }
+
+    private boolean checkUpLeft(int xinMap,int yinMap){
+        int x=xinMap-1;
+        int y=yinMap-1;
+
+        if(y<0||x<0){
+            return true;
+            //위가 나 위옆에 없음
+        }
+
+        if(totalMap[y][x]==2||totalMap[y][x]==3){
+            return checkForBeam(x,y);
+            // 위옆이 보일시에만 검사
+        }else{
+            return true;
+        }
+
+
+
+    }
+
+
+    private boolean isBeamRemovable(int xinMap,int yinMap){
+
+        return checkRight(xinMap,yinMap)&&checkLeft(xinMap,yinMap)&&checkCurrent(xinMap,yinMap);
+
+
+    }
+
+    private boolean checkRight(int xinMap,int yinMap){
+        int x=xinMap+1;
+        int y=yinMap;
+
+        if(x>N){
+            return true;
+            //오른쪽이 없음
+        }
+
+
+        if(totalMap[y][x]==1){
+            return checkForColumn(x,y);
+            // 옆에 기둥일시
+        }else if(totalMap[y][x]==2||totalMap[y][x]==3){
+            return checkForBeam(x,y);
+            //옆에 보일시
+        }else{
+            return true;
+            //옆에 아무것도 없음
+
+        }
+
+
+
+    }
+
+    private boolean checkLeft(int xinMap,int yinMap){
+        int x=xinMap-1;
+        int y=yinMap;
+
+        if(x<0){
+            return true;
+            //왼쪽이 없음
+        }
+
+
+        if(totalMap[y][x]==2||totalMap[y][x]==3){
+            return checkForBeam(x,y);
+            // 왼쪽 옆이 보나 두가 다 있을시
+        }else{
+            return true;
+            //옆에 아무것도없거나 기둥만 있을시
+
+        }
+
+
+
+    }
+    private boolean checkCurrent(int xinMap,int yinMap){
+        int x=xinMap;
+        int y=yinMap;
+
+
+        if(totalMap[y][x]==1){
+            return checkForColumn(x,y);
+            // 자기자리에 지우기전에 둘다 잇으시 그러니까 현재 기둥이 있을시시
+       }else{
+            return true;
+            //옆에 아무것도없거나 기둥만 있을시
+
+        }
     }
 
 
     private void construct(int[] frames){
        if(isConstructable(frames)) {
-           Material material=create(frames);
+           Material material=getMaterial(frames);
+           markInMap(frames);
            constructed.offer(material);
-           map[N-frames[1]][frames[0]]=material.getType()+1;
-           // mark in map
        }
 
     }
 
-    private Material create(int[] frames){
-        if(frames[2]==0) return new Column(frames[0],frames[1]);
+    private void markInMap(int[] frames){
+        markInMap(frames,1);
+    }
+    private void removeMarkInMap(int[] frames){
+        markInMap(frames,-1);
+    }
 
+    private void markInMap(int[] frames,int num){
+
+        totalMap[N-frames[1]][frames[0]]=totalMap[N-frames[1]][frames[0]]+((frames[2]+1)*num);
+        if(frames[2]==0) {
+            columnMap[N-frames[1]][frames[0]]=columnMap[N-frames[1]][frames[0]]+num;
+            return;
+        }
+       beamMap[N-frames[1]][frames[0]]=beamMap[N-frames[1]][frames[0]]+num;
+
+    }
+
+
+
+
+    private Material getMaterial(int[] frames){
+        if(frames[2]==0)  return new Column(frames[0],frames[1]);
         return new Beam(frames[0],frames[1]);
-
     }
 
     private void remove(int[] frames){
 
-        if(isRemovable(frames)){
-            constructed.remove(create(frames));
-            map[N-frames[1]][frames[0]]=0;
+        removeMarkInMap(frames);
 
+        if(isRemovable(frames)){
+
+             constructed.remove(getMaterial(frames));
+
+             return;
         }
+
+
+        markInMap(frames);
+
+
+
     }
 
     public int[][] convertToArray(){
@@ -115,6 +281,8 @@ public class ColumnAndBeam {
         }
 
         return result;
+
+
     }
 
 
--- src/test/ColumnAndBeamTest.java
+++ src/test/ColumnAndBeamTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.PriorityQueue;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class ColumnAndBeamTest {
 
@@ -14,30 +15,34 @@ public class ColumnAndBeamTest {
 
         ColumnAndBeam test = new ColumnAndBeam();
         int[][] input= new int[][] {
-                {0,1,0,1},
-                {0,2,0,1},
+                {1,0,0,1},
                 {1,1,1,1},
-                {1,2,0,1},
-                {2,1,1,1},
-                {2,2,0,1},
-                {5,1,1,1},
-                {0,2,0,0},
-                {2,1,1,0},
+                {2,1,0,1},
+                {2,2,1,1},
+                {5,0,0,1},
+                {5,1,0,1},
+                {4,2,1,1},
+                {3,2,1,1}
+
         };
 
-        int[][] arrayTest= test.solution(5,input);
+        int[][] result= test.solution(5,input);
 
           int[][] expected= new int[][] {
-                  {0,1,0},
+                  {1,0,0},
                   {1,1,1},
-                  {1,2,0},
-//                  {2,1,1},
-                  {2,2,0},
-                  {5,1,1},
+                  {2,1,0},
+                 {2,2,1},
+                  {3,2,1},
+                  {4,2,1},
+                  {5,0,0},
+                  {5,1,0},
           };
 
-          for(int i=0;i<arrayTest.length;i++){
-              assertArrayEquals(arrayTest[i],expected[i]);
+
+
+          for(int i=0;i<result.length;i++){
+              assertArrayEquals(expected[i],result[i]);
           }
 
 
@@ -47,4 +52,133 @@ public class ColumnAndBeamTest {
 
     }
 
+    @Test
+    public void test2(){
+        ColumnAndBeam test = new ColumnAndBeam();
+        int[][] input= new int[][] {
+                {0,0,0,1},
+                {2,0,0,1},
+                {4,0,0,1},
+                {0,1,1,1},
+                {1,1,1,1},
+                {2,1,1,1},
+                {3,1,1,1},
+                {2,0,0,0},
+                {1,1,1,0},
+                {2,2,0,1}
+
+        };
+
+        int[][] result= test.solution(5,input);
+
+        int[][] expected= new int[][] {
+                {0,0,0},
+                {0,1,1},
+                {1,1,1},
+                {2,1,1},
+                {3,1,1},
+                {4,0,0}
+
+        };
+
+        for(int i=0;i<expected.length;i++){
+            System.out.println(result[i][0]+","+result[i][1]+","+result[i][2]);
+            assertArrayEquals(expected[i],result[i]);
+        }
+
+
+
+    }
+
+
+    @Test
+    public void test3(){
+        ColumnAndBeam test = new ColumnAndBeam();
+        int[][] input= new int[][] {
+                {1,0,0,1},
+                {1,5,0,1},
+                {4,5,1,1},
+                {1,1,1,1},
+                {2,1,0,1},
+                {2,2,0,1},
+                {1,2,0,1},
+                {1,3,1,1},
+                {2,3,0,1},
+                {0,0,0,1},
+                {0,1,0,1},
+                {0,2,1,1},
+                {1,2,0,1},
+                {2,2,0,0},
+
+        };
+
+        int[][] result= test.solution(5,input);
+
+        int[][] expected= new int[][] {
+                {0,0,0},
+                {0,1,0},
+                {0,2,1},
+                {1,0,0},
+                {1,1,1},
+                {1,2,0},
+                {1,3,1},
+                {2,1,0},
+                {2,3,0}
+
+        };
+
+
+        for(int i=0;i<expected.length;i++){
+            System.out.println(result[i][0]+","+result[i][1]+","+result[i][2]);
+            assertArrayEquals(expected[i],result[i]);
+        }
+
+
+
+    }
+
+
+
+    @Test
+    public void test4(){
+
+        ColumnAndBeam test = new ColumnAndBeam();
+        int[][] input= new int[][] {
+                {1,0,0,1},
+                {1,1,1,1},
+                {2,1,0,1},
+                {2,2,1,1},
+                {5,0,0,1},
+                {5,1,0,1},
+                {4,2,1,1},
+                {3,2,1,1},
+                {3,2,1,0},
+                {2,2,1,0},
+                {2,1,0,0},
+                {1,1,1,0},
+                {1,0,0,0},
+                {4,2,1,0},
+                {5,1,0,0},
+                {5,0,0,0},
+
+
+        };
+
+        int[][] result= test.solution(5,input);
+
+//        assertEquals(0,result.length);
+        for(int i=0;i<result.length;i++){
+            System.out.println(result[i][0]+","+result[i][1]+","+result[i][2]);
+        }
+          int[][] expected= new int[][]{};
+        assertArrayEquals(expected,result);
+//
+//
+//
+
+
+
+
+
+    }
 }
